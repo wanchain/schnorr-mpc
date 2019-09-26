3376fb9776fb537e59e0667021d5494c42a130b5
JeremyJiaqg <185356122@qq.com>
2019-09-25 20:05:02 -0700

    changed func

--- internal/jsre/jsre.go
+++ internal/jsre/jsre.go
@@ -32,8 +32,8 @@ import (
 )
 
 var (
-	BigNumber_JS = deps.Asset("bignumber.js")
-	Web3_JS = deps.Asset("web3.js")
+	BigNumber_JS= deps.MustAsset("bignumber.js")
+	Web3_JS  = deps.MustAsset("web3.js")
 )
 
 /*
