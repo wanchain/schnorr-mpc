def5a446752de50cb223d1f66013203332ffe46b
JeremyJiaqg <185356122@qq.com>
2019-09-29 09:31:23 +0800

    removed directory

--- sigVerify/Wanchain/migrations/2_deploy_contracts.js
+++
@@ -1,8 +0,0 @@
-// const HTLCETH = artifacts.require("./HTLCETH.sol");
-// const ConvertLib = artifacts.require("./ConvertLib.sol")
-
-module.exports = function(deployer, network, accounts) {
-	// deployer.deploy(ConvertLib);
-	// deployer.autolink();
-	// deployer.deploy(HTLCETH);
-};
